527b4f228cd9369a24aa98cbb9dcb9aa0d3c4f50
Surya Manavalan <suryamnvln@gmail.com>
2025-09-29 16:49:00 -0700

    Add list of messages

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 const messageElement = document.getElementById("message");
 
-messageElement.innerHTML = "Hello World";
+let messages = [
+    "Hello World",
+    "It's day 2"
+];
+
+messageElement.innerHTML = messages.reverse().map((m) => {
+    return '<p>' + m + '</p>\n\n'
+});

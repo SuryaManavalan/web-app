0a73e690bc765976c9ffc79ca5108a1cfbe70f0a
Surya Manavalan <suryamnvln@gmail.com>
2025-09-29 19:29:35 -0700

    Map output tweet

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const messageElement = document.getElementById("message");
 
 let messages = [
     "Hello World",
-    "It's day 2"
+    "It's day 2",
+    "I think I understand map outputs now."
 ];
 
 messageElement.innerHTML = messages.reverse().map((m) => {
